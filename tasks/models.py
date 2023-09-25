2e977ef1308c360d1dec5889bd0978fc599bba83
moinul75 <moinul1955198@gmail.com>
2023-09-25 12:37:01 +0600

    adding model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+PRIORITY_CHOICES = (
+    ('low', 'Low'),
+    ('medium', 'Medium'),
+    ('high', 'High'),
+)
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    due_date = models.DateField()
+    priority = models.CharField(max_length=20, choices=PRIORITY_CHOICES)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+# adding  a model for task photos
+class TaskPhoto(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='photos')
+    photo = models.ImageField(upload_to='task_photos/') 
+
+    def __str__(self):
+        return f"Photo for {self.task.title}"
