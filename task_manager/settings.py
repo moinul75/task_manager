ac46f42b5093bc9e1bc4c7bd86c4bf4eeee57018
moinul75 <moinul1955198@gmail.com>
2023-09-25 12:49:56 +0600

    config static and media url

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -140,6 +140,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFIELS_DIRS = [
+    BASE_DIR /'static'
+]
+
+#meida files 
+MEDIA_URL = 'media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
